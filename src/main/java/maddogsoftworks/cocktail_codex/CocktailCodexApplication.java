ded6c2591b9230a197d87717f79186a6c9fbff83
Rob MacPherson <61522594+RobLTMacP@users.noreply.github.com>
2025-02-16 10:14:02 -0500

    first init

---
+++ src/main/java/maddogsoftworks/cocktail_codex/CocktailCodexApplication.java
@@ -0,0 +1,13 @@
+package maddogsoftworks.cocktail_codex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailCodexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailCodexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maddogsoftworks/cocktail_codex/CocktailCodexApplicationTests.java
@@ -0,0 +1,13 @@
+package maddogsoftworks.cocktail_codex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailCodexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
